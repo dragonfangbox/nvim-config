4b7cceb60b3cf5102aaa398356a5ccf595738441
dragonfangbox <josiah@jheslop.com>
2025-09-06 16:24:46 -0400

    changed it so enter completes autocompletes

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,29 +1,24 @@
 return {
 	{"hrsh7th/nvim-cmp", config = function()
-		require'cmp'.setup {
-			snippet = {
-				expand = function() end,
-			},
-
+		local cmp = require'cmp'
+		cmp.setup {
 			completion = {
-					completeopt = 'menuone, noselect',
+					completeopt = 'menuone, noinsert',
 				},
 
 			window = {
-				documentation = require'cmp'.config.window.bordered()
+				documentation = cmp.config.window.bordered()
 
 			},
 			mapping = {
-			  ['<Down>'] = require'cmp'.mapping.select_next_item(),
-			  ['<Up>'] = require'cmp'.mapping.select_prev_item(),
-			  ['<C-d>'] = require'cmp'.mapping.scroll_docs(4),
-			  ['<C-u>'] = require'cmp'.mapping.scroll_docs(-4),
-			  ['<C-Space>'] = require'cmp'.mapping.complete(),
-			  ['<CR>'] = require'cmp'.mapping.confirm({ select = true }),
+			  ['<Tab>'] = cmp.mapping.select_next_item(),
+			  ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+			  ['<CR>'] = cmp.mapping.confirm({ select = true }),
 			},
 			sources = {
 			  { name = 'nvim_lsp' },
-	--		  { name = 'path' },
+			  { name = 'buffer' },
+			  { name = 'path' },
 			},
 			experimental = {
 				ghost_text = true,
